1d7533db1876204ef2f7f62b6785050acb23a286
Daniel Frota <danielfrot@gmail.com>
2022-11-10 20:43:33 -0400

    ajustando a imagem

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -4,16 +4,16 @@ import Card from 'react-bootstrap/Card';
 
 function MyCard({nome}) {
   return (
-  <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="image.jpeg" />
-  <Card.Body>
-    <Card.Title>{nome}</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the
-      bulk of the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
+  <Card style={{ width: '18rem', display: "flex", alignItems: 'center' }}>
+    <Card.Img variant="top" src="image.jpeg" style={{width: 120}} />
+    <Card.Body>
+      <Card.Title>{nome}</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
 </Card>
 )
 }
